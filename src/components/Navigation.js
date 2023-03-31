b0b06648ff5dce9442d42f07aa9910d80a1bc17b
Ismail Ali <ismailchangezi801@gmail.com>
2023-03-31 13:39:11 +0500

    Create Navigation for the page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <header>
+      <Link to="/">
+        <h1>BookStore CMS</h1>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Categories">Categories</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
