452fbab97e59e098615b4538dca9ff7d60ba29a3
Ismail Ali <ismailchangezi801@gmail.com>
2023-04-05 16:50:34 +0500

    Use dispatch and useSelector to render the store data

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,29 +1,31 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import booksSlice, { removeBook } from "../redux/books/booksSlice";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
 export default function Book() {
   const list = useSelector((state) => state.items);
   const dispatch = useDispatch();
+
   return (
     <section>
       <ul>
-        {list.map((items) => {
-          return (
-            <div key={items.id}>
-              <h1>{items.title}</h1>
-              <p>by {items.author}</p>
-              <button
-                onClick={() => {
-                  dispatch(removeBook(items.id));
-                }}
-              >
-                remove book
-              </button>
-            </div>
-          );
-        })}
+        {list.map((items) => (
+          <div key={items.id}>
+            <h1>{items.title}</h1>
+            <p>
+              by
+              {items.author}
+            </p>
+            <button
+              type="submit"
+              onClick={() => {
+                dispatch(removeBook(items.id));
+              }}
+            >
+              remove book
+            </button>
+          </div>
+        ))}
       </ul>
     </section>
   );
